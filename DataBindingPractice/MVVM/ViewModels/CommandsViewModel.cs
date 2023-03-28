f0aa8f682d7d2e5685ecff93e18034a8dcdc25b5
rushi-smb <rushi@smbios.co.in>
2023-03-28 23:38:13 +0530

    Display Alert on Button Click using Command from ViewModel used ICommand

--- DataBindingPractice/MVVM/ViewModels/CommandsViewModel.cs
+++ DataBindingPractice/MVVM/ViewModels/CommandsViewModel.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace DataBindingPractice.MVVM.ViewModels
 {
     public class CommandsViewModel
     {
+        public ICommand ClickCommand =>
+            new Command(() => 
+            App.Current.MainPage
+            .DisplayAlert("Title", "Message", "Ok"));
     }
 }
