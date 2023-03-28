b9146303a5323c13b3424b6cd5c260b2975786ab
rushi-smb <rushi@smbios.co.in>
2023-03-28 20:33:59 +0530

    Implement IValueConverter Interface to convert Value from string to Bool

--- DataBindingPractice/MVVM/Converters/BoolConverter.cs
+++ DataBindingPractice/MVVM/Converters/BoolConverter.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataBindingPractice.MVVM.Converters
 {
-    public class BoolConverter
+    public class BoolConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
