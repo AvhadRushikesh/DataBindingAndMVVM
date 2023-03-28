11016d2b40f58c1351c060335a101810c443ca95
rushi-smb <rushi@smbios.co.in>
2023-03-28 18:57:18 +0530

    Add PeopleViewModel and Bind in PeopleView.xaml.cs file

--- DataBindingPractice/MVVM/Views/PeopleView.xaml.cs
+++ DataBindingPractice/MVVM/Views/PeopleView.xaml.cs
@@ -1,3 +1,5 @@
+using DataBindingPractice.MVVM.ViewModels;
+
 namespace DataBindingPractice.MVVM.Views;
 
 public partial class PeopleView : ContentPage
@@ -5,5 +7,7 @@ public partial class PeopleView : ContentPage
 	public PeopleView()
 	{
 		InitializeComponent();
+
+		BindingContext = new PeopleViewModel();
 	}
 }
