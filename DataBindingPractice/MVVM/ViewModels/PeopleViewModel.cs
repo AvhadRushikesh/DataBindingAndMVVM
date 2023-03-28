f9a902914908270e5f0244831fdb727471ba7d50
rushi-smb <rushi@smbios.co.in>
2023-03-28 20:09:37 +0530

    Add Date to the List<Person> for Dynamic Binding

--- DataBindingPractice/MVVM/ViewModels/PeopleViewModel.cs
+++ DataBindingPractice/MVVM/ViewModels/PeopleViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataBindingPractice.MVVM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +9,55 @@ namespace DataBindingPractice.MVVM.ViewModels
 {
     public class PeopleViewModel
     {
-        public List<string> People { get; set; }
+        public List<Person> People { get; set; } = new List<Person>();
 
         public PeopleViewModel()
         {
-            People = new List<string>
-            {
-                "Tony",
-                "Steve",
-                "Thor",
-                "Natasha",
-                "Clint",
-                "Bruce"
-            };
+            People.Add(new Person() 
+            { 
+                Name = "John", 
+                Age = 42, 
+                Married = true, 
+                BirthDate = new DateTime(1980, 1, 1), 
+                Weight = 80, 
+                LunchTime = new TimeSpan(12, 30, 0) 
+            });
+            People.Add(new Person() 
+            { 
+                Name = "Mary",
+                Age = 35,
+                Married = false, 
+                BirthDate = new DateTime(1980, 1, 1),
+                Weight = 60, 
+                LunchTime = new TimeSpan(13, 30, 0) 
+            });
+            People.Add(new Person() 
+            { 
+                Name = "Bob",
+                Age = 25,
+                Married = true,
+                BirthDate = new DateTime(1980, 1, 1), 
+                Weight = 40, 
+                LunchTime = new TimeSpan(14, 30, 0) 
+            });
+            People.Add(new Person()
+            { 
+                Name = "Jane",
+                Age = 22,
+                Married = false,
+                BirthDate = new DateTime(1980, 1, 1),
+                Weight = 30,
+                LunchTime = new TimeSpan(15, 30, 0)
+            });
+            People.Add(new Person() 
+            { 
+                Name = "Jack", 
+                Age = 18, 
+                Married = true, 
+                BirthDate = new DateTime(1980, 1, 1),
+                Weight = 20, 
+                LunchTime = new TimeSpan(16, 30, 0)
+            });
         }
     }
 }
