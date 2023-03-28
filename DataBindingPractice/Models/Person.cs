61dbfa847f6653478f79d85c6bdcd61bf84fe4a1
rushi-smb <rushi@smbios.co.in>
2023-03-28 15:17:40 +0530

    Implement INotifyPropertyChanged Interface in Person Model

--- DataBindingPractice/Models/Person.cs
+++ DataBindingPractice/Models/Person.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataBindingPractice.Models
 {
-    public class Person
+    public class Person : INotifyPropertyChanged
     {
         public string Name { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
