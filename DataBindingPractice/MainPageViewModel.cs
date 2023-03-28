0b685adfd38a1940f0b598b48224c6615bda2986
rushi-smb <rushi@smbios.co.in>
2023-03-29 00:11:51 +0530

    Add Entry and Button Control and Binding

---
+++ DataBindingPractice/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DataBindingPractice
+{
+    public class MainPageViewModel
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+        public int Result { get; set; }
+
+        public ICommand AddCommand =>
+            new Command(() => Result = Number1 + Number2);
+    }
+}
