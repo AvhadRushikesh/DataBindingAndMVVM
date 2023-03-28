7ddcc88bd727432f2f3ca368ceb7a6bf3e9a5ce3
rushi-smb <rushi@smbios.co.in>
2023-03-28 17:19:02 +0530

    Minor Changes in Main Page xaml file to remove error

--- DataBindingPractice/MainPage.xaml.cs
+++ DataBindingPractice/MainPage.xaml.cs
@@ -1,32 +1,14 @@
-﻿using DataBindingPractice.Models;
+﻿
 
 namespace DataBindingPractice;
 
 public partial class MainPage : ContentPage
 {
 
-	Person person = new Person();
+	
 
 	public MainPage()
 	{
 		InitializeComponent();
-
-        person = new Person
-        {
-            Name = "John",
-            Phone = "9999",
-            Address = "X Address"
-        };
-
-        BindingContext = person;
     }
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-        person.Name = "Peter";
-        person.Phone = "0000";
-        person.Address = "New Address";
-
-
-	}
 }
