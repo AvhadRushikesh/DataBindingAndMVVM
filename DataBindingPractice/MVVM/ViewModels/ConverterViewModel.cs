de45c9c433654092a5c810ac420ceed13dbb0940
rushi-smb <rushi@smbios.co.in>
2023-03-28 20:23:46 +0530

    Create ConverterViewModel to Convert from String to Bool

---
+++ DataBindingPractice/MVVM/ViewModels/ConverterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using DataBindingPractice.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingPractice.MVVM.ViewModels
+{
+    public class ConverterViewModel
+    {
+        public Data data { get; set; }
+
+        public ConverterViewModel()
+        {
+            data = new Data
+            {
+                Married = "YES"
+            };
+        }
+    }
+}
