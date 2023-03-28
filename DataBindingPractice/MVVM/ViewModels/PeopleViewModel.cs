df2cacf981d0e411c87ba7f7f9fdfd739edffdc0
rushi-smb <rushi@smbios.co.in>
2023-03-28 20:17:02 +0530

    Binding Name from List<ModelName>

--- DataBindingPractice/MVVM/ViewModels/PeopleViewModel.cs
+++ DataBindingPractice/MVVM/ViewModels/PeopleViewModel.cs
@@ -13,35 +13,35 @@ namespace DataBindingPractice.MVVM.ViewModels
 
         public PeopleViewModel()
         {
-            People.Add(new Person() 
-            { 
-                Name = "John", 
-                Age = 42, 
-                Married = true, 
-                BirthDate = new DateTime(1980, 1, 1), 
-                Weight = 80, 
-                LunchTime = new TimeSpan(12, 30, 0) 
+            People.Add(new Person()
+            {
+                Name = "John",
+                Age = 42,
+                Married = true,
+                BirthDate = new DateTime(1980, 1, 1),
+                Weight = 80,
+                LunchTime = new TimeSpan(12, 30, 0)
             });
-            People.Add(new Person() 
-            { 
+            People.Add(new Person()
+            {
                 Name = "Mary",
                 Age = 35,
-                Married = false, 
+                Married = false,
                 BirthDate = new DateTime(1980, 1, 1),
-                Weight = 60, 
-                LunchTime = new TimeSpan(13, 30, 0) 
+                Weight = 60,
+                LunchTime = new TimeSpan(13, 30, 0)
             });
-            People.Add(new Person() 
-            { 
+            People.Add(new Person()
+            {
                 Name = "Bob",
                 Age = 25,
                 Married = true,
-                BirthDate = new DateTime(1980, 1, 1), 
-                Weight = 40, 
-                LunchTime = new TimeSpan(14, 30, 0) 
+                BirthDate = new DateTime(1980, 1, 1),
+                Weight = 40,
+                LunchTime = new TimeSpan(14, 30, 0)
             });
             People.Add(new Person()
-            { 
+            {
                 Name = "Jane",
                 Age = 22,
                 Married = false,
@@ -49,13 +49,13 @@ namespace DataBindingPractice.MVVM.ViewModels
                 Weight = 30,
                 LunchTime = new TimeSpan(15, 30, 0)
             });
-            People.Add(new Person() 
-            { 
-                Name = "Jack", 
-                Age = 18, 
-                Married = true, 
+            People.Add(new Person()
+            {
+                Name = "Jack",
+                Age = 18,
+                Married = true,
                 BirthDate = new DateTime(1980, 1, 1),
-                Weight = 20, 
+                Weight = 20,
                 LunchTime = new TimeSpan(16, 30, 0)
             });
         }
