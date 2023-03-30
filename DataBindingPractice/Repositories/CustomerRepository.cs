76054ebcb23b7320ed4aa9f11647376025182f09
rushi-smb <rushi@smbios.co.in>
2023-03-30 13:39:11 +0530

    Access database SQL Connection

--- DataBindingPractice/Repositories/CustomerRepository.cs
+++ DataBindingPractice/Repositories/CustomerRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace DataBindingPractice.Repositories
 {
     public class CustomerRepository
     {
+        SQLiteConnection conn;
+
+        public CustomerRepository()
+        {
+            conn = new SQLiteConnection(Constants.DatabasePath, Constants.Flags);
+        }
     }
 }
